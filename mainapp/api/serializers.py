3f1f35a284d0741a50a9ba144e2145879f292a70
vvp1488 <drozdik24011994@gmail.com>
2021-08-20 17:13:48 +0300

    delete some

--- mainapp/api/serializers.py
+++ mainapp/api/serializers.py
@@ -4,10 +4,6 @@ from mainapp.models import UserModel
 
 
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField()
-    password = serializers.CharField(max_length=14)
-    first_name = serializers.CharField(max_length=100)
-    last_name = serializers.CharField(max_length=100)
 
     class Meta:
         model = UserModel
